bcab3838bdd2696b2388a53c8c3e1065ab131af7
Thiago <tsc.carvalho@mat.ci.ufpb.br>
2019-06-12 18:33:49 -0300

    The tutorial was finish. Good image of slash. hehe

--- custom_clipper/lib/main.dart
+++ custom_clipper/lib/main.dart
@@ -2,9 +2,60 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        body: ClipPath(
+          clipper: MyClipper(),
+          child: Container(
+            height: 350.0,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                colorFilter: ColorFilter.mode(
+                  Colors.purpleAccent.withOpacity(0.3),
+                  BlendMode.color,
+                ),
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                "https://guitar.com/wp-content/uploads/2019/04/Interview-Slash-Apollo-live-hero@1400x1050.jpg"
+              )),
+            )
+          ),
+        ),
+      ),
+    );
   }
+}
+
+class MyClipper extends CustomClipper<Path> {
+
+  // get the Clip whenever its needs to update
+  // size here is really the size of CustomClipper's child
+  @override
+  Path getClip(Size size) {
+    Path p = Path();
+
+    // p(x,y)
+    // must to be in order! The lineTo function start from the last line
+    p.lineTo(0, size.height);
+    p.lineTo(size.width, size.height / 1.2);
+    p.lineTo(size.width, 0);
+
+
+    return p;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    return false;
+  }
+
 }
